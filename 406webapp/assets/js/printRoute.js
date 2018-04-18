93a937c579671ffa35256e87b2dbe98a616455e2
Edward Zapanta <zapanta.ed@gmail.com>
2018-04-18 12:49:47 -0400

    Deleted picRoute js file
    Deleted picRoute as it is useless.

--- 406webapp/assets/js/printRoute.js
+++ 406webapp/assets/js/printRoute.js
@@ -1,19 +1,17 @@
-
-
 function getRoute() {
-	var imagePath = "/assets/images/"
+	var imagePath = "assets/images/"
 	var startingPoint = document.getElementById("start").value;
 	var destination = document.getElementById("end").value;
 	var start = startingPoint.concat("_");
+	var navigationOption = document.getElementById("access").value;
 	
-	var navigationOption = document.getElementById("accessibility").value;
-	if (navigationOption = "elevator"){
+	if (navigationOption == "elevator"){
 		imagePath = imagePath.concat("elevator/");
 	} else {
 		imagePath = imagePath.concat("stairs/");
 	}
+	
 	var pictureName = start.concat(destination);
-	document.getElementById('floor1').src = imagePath + pictureName +".png" + new Date().getTime();
-	}
+	var imagePath = imagePath.concat(pictureName);
+	document.getElementById('floor1').src = imagePath +".png" ;
 }
-
